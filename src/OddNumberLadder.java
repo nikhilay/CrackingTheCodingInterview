b25e18f5879ca9242651b4f3910dd56715b943c6
nikhilay <nikhilayyagari15@gmail.com>
2016-09-29 16:50:56 -0400

    Learnt about continue label statement and usage of it

---
+++ src/OddNumberLadder.java
@@ -0,0 +1,48 @@
+/**
+ * Created by Nikhil on 9/29/16.
+ */
+public class OddNumberLadder {
+
+    public static void main(String[] args) {
+        new OddNumberLadder().generateLadder(10);
+        new OddNumberLadder().createLadder(10);
+    }
+
+    // Displaying the useage of continue with label statement
+    public void generateLadder(int input) {
+        outerLabel:
+        for (int x = 1; x > 0; x++) {
+            innerLabel:
+            for (int y = 1; y <= x; y++) {
+                if (x > input) {
+                    break outerLabel;
+                }
+
+                if (x % 2 == 0) {
+                    continue outerLabel;
+                }
+
+                if (y % 2 == 0) {
+                    continue innerLabel;
+                }
+
+                System.out.print(y + " ");
+            }
+            System.out.print("\n");
+        }
+
+    }
+
+    //Without usage of continue label statement
+    public void createLadder(int input) {
+
+        for (int x = 1; x <= input; x = x + 2) {
+            for (int y = 1; y <= x; y = y + 2) {
+                System.out.print(y + " ");
+            }
+            System.out.print("\n");
+
+        }
+
+    }
+}
