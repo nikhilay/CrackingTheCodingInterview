4174b51eb28a08e97c0dd53af9a9eaad2029a642
nikhilay <nikhilayyagari15@gmail.com>
2016-10-06 00:02:41 -0400

    Added a better solution for add digit

--- src/LeetCodeAddDigit.java
+++ src/LeetCodeAddDigit.java
@@ -1,12 +1,17 @@
+import java.security.Timestamp;
+import java.util.Calendar;
+
 /**
  * Created by Nikhil on 9/22/16.
  */
 public class LeetCodeAddDigit {
     public static void main(String[] args){
         LeetCodeAddDigit ob = new LeetCodeAddDigit();
-        System.out.println(ob.addDigits(456));
+        System.out.println(ob.addDigits(323));
+        System.out.println(ob.efficientAddDigits(323));
     }
     public int addDigits(int num) {
+
         int result = 0;
 
         while(num!=0){
@@ -22,5 +27,14 @@ public class LeetCodeAddDigit {
             return result;
         }
 
+    }
+
+    //betterSolution
+    public int efficientAddDigits(int input){
+        if(input % 9 ==0){
+            return 9;
+        }else{
+            return input % 9;
+        }
     }
 }
