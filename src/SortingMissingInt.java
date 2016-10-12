1b77fcc8f600368db38c742e2ba5db9f7a0bfb8d
nikhilay <nikhilayyagari15@gmail.com>
2016-10-12 17:30:21 -0400

    CTC 10.7 Missing Int Solution

---
+++ src/SortingMissingInt.java
@@ -0,0 +1,30 @@
+import java.io.*;
+
+import static javafx.scene.input.KeyCode.F;
+
+/**
+ * Created by Nikhil on 10/12/16.
+ */
+public class SortingMissingInt {
+
+    public void findMissingInt() throws IOException {
+        byte[] bitfield = new byte[(int) (Integer.MAX_VALUE / 8) + 1];
+        File f = new File("test1.txt");
+        BufferedReader br = new BufferedReader(new FileReader(f));
+        String line = null;
+        while ((line = br.readLine()) != null) {
+            int n = Integer.valueOf(line);
+            bitfield[n / 8] |= 1 << (n % 8);
+        }
+        br.close();
+
+        for (int i = 0; i < bitfield.length; i++) {
+            for (int j = 0; j < 8; j++) {
+                if ((bitfield[i] & 1 << j) == 0) {
+                    System.out.println("Number is " + i * 8 + j);
+                    return;
+                }
+            }
+        }
+    }
+}
