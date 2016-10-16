7ddb776c3f5a0f5686b488d7fec04e541b3a983f
nikhilay <nikhilayyagari15@gmail.com>
2016-10-16 17:38:12 -0400

    Solution to number of ways to to place N Queen

--- src/DPNQueenProblem.java
+++ src/DPNQueenProblem.java
@@ -1,6 +1,8 @@
 import javafx.geometry.Pos;
 import org.omg.Messaging.SYNC_WITH_TRANSPORT;
 
+import java.util.ArrayList;
+
 /**
  * Created by Nikhil on 10/16/16.
  */
@@ -15,23 +17,24 @@ public class DPNQueenProblem {
         }
     }
 
-    public static void main(String[] args){
-        new DPNQueenProblem().solveNQueenProblem(8);
+    public static void main(String[] args) {
+       //new DPNQueenProblem().solveNQueenProblem(8);
+        new DPNQueenProblem().noOfWays(4);
     }
 
     public void solveNQueenProblem(int n) {
         Position[] positions = new Position[n];
         boolean isPlacingPossible = solveNQueenProblem(0, n, positions);
-        if(isPlacingPossible){
+        if (isPlacingPossible) {
             System.out.println("Below are the positions where queens wont attack each other");
-            int q =1;
-            for(Position p:positions){
-                System.out.println("Queen - "+q+" "+"("+p.row+", "+p.col+" )");
+            int q = 1;
+            for (Position p : positions) {
+                System.out.println("Queen - " + q + " " + "(" + p.row + ", " + p.col + " )");
                 q++;
             }
 
-        }else{
-            System.out.println("We cannot place "+n+" - queen in "+n+" x "+ n+" chessboard");
+        } else {
+            System.out.println("We cannot place " + n + " - queen in " + n + " x " + n + " chessboard");
         }
 
 
@@ -59,6 +62,40 @@ public class DPNQueenProblem {
         }
 
         return false;
+    }
+    //To find all possible position to place N-Queen
+
+
+    public void noOfWays(int n) {
+        ArrayList<Position[]> result = new ArrayList<Position[]>();
+        Position[] positions = new Position[n];
+        findAllNQueenPositions(0, n, positions, result);
+        System.out.println("No of possible ways is " + result.size());
+    }
+
+    private void findAllNQueenPositions(int row, int noOfQueen, Position[] positions, ArrayList<Position[]> result) {
+        if (row == noOfQueen) {
+            result.add(positions);
+            return;
+
+        }
+        for (int col = 0; col < noOfQueen; col++) {
+            boolean isItSafe = true;
+            for (int queen = 0; queen < row; queen++) {
+                if (positions[queen].col == col || positions[queen].row == row ||
+                        (positions[queen].row - positions[queen].col == row - col) ||
+                        (positions[queen].row + positions[queen].col == row + col)) {
+
+                    isItSafe = false;
+                    break;
+                }
+            }
+            if (isItSafe) {
+                positions[row] = new Position(row, col);
+                findAllNQueenPositions(row + 1, noOfQueen, positions, result);
+            }
+        }
+
     }
 
 }
