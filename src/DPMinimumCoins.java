f835e09bd20ed9c9a1bcca9e33527d73a181a6fe
nikhilay <nikhilayyagari15@gmail.com>
2016-10-16 14:19:29 -0400

    DP Find minimum number of coins to make a sum Solution

--- src/DPMinimumCoins.java
+++ src/DPMinimumCoins.java
@@ -1,47 +1,45 @@
-import java.util.HashMap;
-
-import static com.sun.tools.javac.jvm.ByteCodes.ret;
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
 
 /**
  * Created by Nikhil on 10/14/16.
  */
 public class DPMinimumCoins {
 
-//    public static void main(String[] args) {
-//
-//    }
-//
-//    public int minimumNoOfCoins(int[] denomination,int v) {
-//        int[] memo = new int[v+1];
-//        memo[0] =0;
-//        for(int i=1;i<memo.length;i++){
-//            int min;
-//            for(int d:denomination){
-//               if((i-d)>=0){
-//                   min = memo[i-d]+1;
-//               }
-//            }
-//        }
-//
-//
-//    }
-
-    public int majorityElement(int[] nums) {
-
-        HashMap<Integer,Integer> hmap = new HashMap<>();
-
-        for(int i:nums){
-            if(hmap.containsKey(i)){
-                int count = hmap.get(i)+1;
-                if((count)>(nums.length/2)){
-                    return i;
-                }
-                hmap.put(i,count);
-
-                continue;
-            }
-            hmap.put(i,1);
-        }
-        return 0;
+    public static void main(String[] args) {
+        int[] denomination = {1, 5, 12, 25};
+        new DPMinimumCoins().minimumNoOfCoins(denomination, 16);
     }
+
+    public void minimumNoOfCoins(int[] denomination, int v) {
+        int[] memo = new int[v + 1];
+        memo[0] = 0;
+        int[] coins = new int[v + 1];
+        coins[0] = 0;
+        for (int i = 1; i < memo.length; i++) {
+            int min = Integer.MAX_VALUE;
+            for (int d : denomination) {
+                if ((i - d) >= 0) {
+                    if((memo[i-d]+1)<min){
+                        min = memo[i-d]+1;
+                        coins[i] = d;
+                    }
+
+                } else {
+                    break;
+                }
+            }
+
+            memo[i] = min;
+
+
+        }
+        System.out.print("Minimum number of coins is " + memo[v] + " for value " + v +" and the coins are ");
+        int value = v;
+        while(value>0){
+            System.out.print( coins[value]+", ");
+            value = value - coins[value];
+        }
+
+    }
+
 }
